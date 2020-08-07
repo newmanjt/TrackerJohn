0210e7714abb72c5b5191fb4ff7bbe5caa386e79
James Newman <newmanjt15@gmail.com>
2020-08-06 19:10:07 -0500

    first commit

---
+++ TrackerJohn.mm
@@ -0,0 +1,129 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1596737357588" ID="ID_413621459" MODIFIED="1596737905493" STYLE="fork" TEXT="TrackerJohn">
+<node CREATED="1596738344189" ID="ID_1032694270" MODIFIED="1596751848222" POSITION="right" TEXT="Triumvirate">
+<node CREATED="1596744599098" HGAP="21" ID="ID_1505406965" MODIFIED="1596745173744" STYLE="bubble" TEXT="Factors should be definable into a set of different categories. Basic categories include the defining triumvirate of human experience: Mind, Body, and Soul" VSHIFT="10">
+<node CREATED="1596744652974" HGAP="54" ID="ID_40199361" MODIFIED="1596745013522" STYLE="fork" TEXT="Mind" VSHIFT="-84">
+<node CREATED="1596744759008" ID="ID_532713713" MODIFIED="1596744852388" STYLE="bubble" TEXT="Mind relates to the aspects of experience corresponding to thought, past experiences, and memories. The Mind contains the past memories of the individual. When absent of direction, the Mind will autonomously flash through memories which will spur thought. ">
+<arrowlink DESTINATION="ID_532713713" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1638157247" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_532713713" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1638157247" SOURCE="ID_532713713" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+<node CREATED="1596747046734" ID="ID_101556720" MODIFIED="1596747054241" TEXT="Example Activities">
+<node CREATED="1596747058948" ID="ID_1436611157" MODIFIED="1596747062407" TEXT="meditation">
+<node CREATED="1596747087943" ID="ID_1879513754" MODIFIED="1596747119434" TEXT="Here is an example where the distinction quickly blurs. Base mindfulness meditation is largely mind, while dancing would verge more to body"/>
+</node>
+<node CREATED="1596747063049" ID="ID_148458340" MODIFIED="1596747068877" TEXT="reading">
+<node CREATED="1596747133467" ID="ID_1820342365" MODIFIED="1596747161161" TEXT="Quality of activity must also play a role. Reading pulp fiction is less positive for the mind than reading essays or nonfiction"/>
+</node>
+<node CREATED="1596747069343" ID="ID_1448164849" MODIFIED="1596747073581" TEXT="work">
+<node CREATED="1596747166181" ID="ID_858415997" MODIFIED="1596747195958" TEXT="What kind of work will force distinctions. Physical labor will sate the Mind less than mental exertion."/>
+</node>
+<node CREATED="1596747202201" ID="ID_1360488269" MODIFIED="1596747203740" TEXT="etc."/>
+</node>
+</node>
+<node CREATED="1596744668153" ID="ID_1777123279" MODIFIED="1596747209101" STYLE="fork" TEXT="Body">
+<node CREATED="1596744930979" ID="ID_1702679804" MODIFIED="1596745099425" STYLE="bubble" TEXT="Body relates the aspects of experience corresponding to our physical world, including, but not limited to, physical sensations such as touch, taste, and smell. The physical world, as we perceive it, can only be accessed through the Body."/>
+<node CREATED="1596747210712" ID="ID_841389011" MODIFIED="1596747214189" TEXT="Example Activities">
+<node CREATED="1596747215154" ID="ID_1154542392" MODIFIED="1596747220462" TEXT="Exercise"/>
+<node CREATED="1596747221277" ID="ID_1477003474" MODIFIED="1596747239490" TEXT="Walking"/>
+<node CREATED="1596747240208" ID="ID_525909041" MODIFIED="1596747245764" TEXT="eating"/>
+<node CREATED="1596747248841" ID="ID_1829143306" MODIFIED="1596747249736" TEXT="etc"/>
+</node>
+</node>
+<node CREATED="1596744669647" HGAP="32" ID="ID_1830763571" MODIFIED="1596745017144" STYLE="fork" TEXT="Soul" VSHIFT="68">
+<node CREATED="1596745019187" ID="ID_249716877" MODIFIED="1596745099427" STYLE="bubble" TEXT="The Soul represents individual human consciousness, often referred to as Me. When considering &quot;My Mind&quot; or &quot;My Body&quot; the Soul concerns the &quot;My&quot; portion of it. Through the Soul we can experience feelings of possession, community, and understand what makes us individuals."/>
+<node CREATED="1596747257868" ID="ID_1960673854" MODIFIED="1596747260858" TEXT="Example Activities">
+<node CREATED="1596747263658" ID="ID_747189528" MODIFIED="1596747265276" TEXT="Prayer"/>
+<node CREATED="1596747265840" ID="ID_1771526042" MODIFIED="1596747276403" TEXT="self interest activities"/>
+<node CREATED="1596747282095" ID="ID_1741714059" MODIFIED="1596747293159" TEXT="volunteering"/>
+<node CREATED="1596747277042" ID="ID_611204248" MODIFIED="1596747278951" TEXT="etc."/>
+</node>
+</node>
+<node CREATED="1596745175186" HGAP="26" ID="ID_771142473" MODIFIED="1596747300406" TEXT="Triumvirate Levels" VSHIFT="57">
+<node CREATED="1596745187360" ID="ID_934117489" MODIFIED="1596745249001" TEXT="Each of the Triumvirate have levels within. It is &quot;Our&quot; responsibility (where Our is the Soul) to recognize which factors of &quot;Ourself&quot; is low and direct our actions accordingly"/>
+</node>
+</node>
+<node CREATED="1596745285481" ID="ID_530948528" MODIFIED="1596745291461" TEXT="Trackable Levels">
+<node CREATED="1596745293023" ID="ID_1515605765" MODIFIED="1596749237810" STYLE="bubble" TEXT="Trackable levels allow each Operator the ability to track how their Triumvirates are doing. It allows the Soul to identify if the Mind is receiving more attention than the body causing an imbalance of power."/>
+<node CREATED="1596747476080" ID="ID_755844738" MODIFIED="1596747500060" TEXT="Imbalances between the Triumvirate can lead to uncontrollable behaviour and acts">
+<node CREATED="1596747507740" ID="ID_1661296066" MODIFIED="1596747525192" TEXT="fidgeting can be caused by the mind having more power to exert than the body"/>
+<node CREATED="1596747914209" ID="ID_638431329" MODIFIED="1596747924661" TEXT="distractions to the mind can be caused by more energy in the soul or body"/>
+</node>
+<node CREATED="1596747535701" ID="ID_1192064536" MODIFIED="1596747546998" TEXT="There exist healthy levels for each person that can lead to better experience">
+<node CREATED="1596747549002" ID="ID_1578701617" MODIFIED="1596747560653" TEXT="the end experience for that person is highly individualized"/>
+<node CREATED="1596747561777" ID="ID_1217449774" MODIFIED="1596747575611" TEXT="the goal of the Triumvirate definition is to lead people to the best balance for themselves"/>
+<node CREATED="1596747576054" ID="ID_1899532525" MODIFIED="1596747602741" TEXT="process of self-exploration is necessary, therefore there will be times where the power becomes unbalanced"/>
+</node>
+<node CREATED="1596747605669" ID="ID_1848677768" MODIFIED="1596747625270" TEXT="An important aspect of the Triumvirate is being able to separate the Triumvirate">
+<node CREATED="1596747626914" ID="ID_1128690449" MODIFIED="1596747658079" TEXT="Being able to separate stems from a developed sense of self and the ability to trim oneself&apos;s actions and map them to different areas"/>
+<node COLOR="#ff0000" CREATED="1596747677175" ID="ID_1725528350" MODIFIED="1596747710707" TEXT="Since each activity is highly individualized, the functionality to create and edit items is a must"/>
+</node>
+</node>
+</node>
+<node CREATED="1596737717090" HGAP="13" ID="ID_1990719621" MODIFIED="1596755318931" POSITION="right" TEXT="Operators" VSHIFT="162">
+<icon BUILTIN="group"/>
+<node CREATED="1596737804793" HGAP="58" ID="ID_930529142" MODIFIED="1596744574154" TEXT="Personal" VSHIFT="-62">
+<node CREATED="1596737725848" ID="ID_841346452" MODIFIED="1596737733444" TEXT="Name"/>
+<node CREATED="1596737793574" ID="ID_326256772" MODIFIED="1596737794688" TEXT="Weight"/>
+<node CREATED="1596737734098" ID="ID_279047196" MODIFIED="1596737793118" TEXT="Height"/>
+<node CREATED="1596737795046" ID="ID_1594247912" MODIFIED="1596737797881" TEXT="Location"/>
+</node>
+<node CREATED="1596737860787" HGAP="70" ID="ID_971523063" MODIFIED="1596744576222" TEXT="Technical Information" VSHIFT="-29">
+<node CREATED="1596737872671" ID="ID_281964079" MODIFIED="1596737876011" TEXT="UserAgents"/>
+<node CREATED="1596737868017" ID="ID_1468163717" MODIFIED="1596737872082" TEXT="IPs"/>
+<node COLOR="#990000" CREATED="1596748731488" ID="ID_1286337957" MODIFIED="1596748737850" TEXT="other collectable info?"/>
+</node>
+<node CREATED="1596737889195" HGAP="74" ID="ID_607895605" MODIFIED="1596744579575" TEXT="ID" VSHIFT="-39"/>
+<node CREATED="1596738053155" HGAP="87" ID="ID_244394439" MODIFIED="1596744581774" TEXT="Login Info" VSHIFT="-33">
+<node CREATED="1596738056155" ID="ID_126816667" MODIFIED="1596738058072" TEXT="username"/>
+<node CREATED="1596738058490" ID="ID_387213454" MODIFIED="1596738059964" TEXT="password"/>
+</node>
+<node CREATED="1596748578473" ID="ID_1924669120" MODIFIED="1596751986540" TEXT="Operations">
+<node CREATED="1596748587649" ID="ID_637689050" MODIFIED="1596749237813" TEXT="Operations are the activities that a Operator can take on each day"/>
+<node CREATED="1596752318249" ID="ID_117607371" MODIFIED="1596752344148" TEXT="Each operation takes a specific amount of time, where unknown is an option"/>
+<node CREATED="1596752346308" ID="ID_1243490372" MODIFIED="1596755176080" TEXT="Each day is composed of N + 1 number of Operations, based on the time available in the day"/>
+<node CREATED="1596755159290" ID="ID_1337242484" MODIFIED="1596755221264" TEXT="The &quot;+ 1&quot; is due to the mandatory Self Criticism Operation done to either compose the current day or next day, and to grade the activities of the day">
+<node CREATED="1596755225444" ID="ID_845626477" MODIFIED="1596755251044" TEXT="This is based on Stoicism, where self-criticism is used as a editorial of sorts "/>
+</node>
+<node CREATED="1596755261620" ID="ID_1654391432" MODIFIED="1596755280174" TEXT="Each Operator must grade operations based on their contributions to the Triumvirate"/>
+</node>
+<node COLOR="#669900" CREATED="1596738897927" HGAP="31" ID="ID_1577220249" MODIFIED="1596748584291" TEXT="Goals" VSHIFT="52">
+<cloud/>
+<node COLOR="#669900" CREATED="1596745432432" ID="ID_933540764" MODIFIED="1596749237813" TEXT="Goals are Operator determined outcomes that are being worked towards"/>
+<node COLOR="#669900" CREATED="1596745456272" ID="ID_901969993" MODIFIED="1596747783966" TEXT="Goals set outcomes of Triumvirate levels"/>
+<node COLOR="#669900" CREATED="1596745472372" ID="ID_1713346279" MODIFIED="1596747783966" TEXT="Goals should be positive in overall experience"/>
+<node COLOR="#669900" CREATED="1596745493149" ID="ID_1482999573" MODIFIED="1596747783965" TEXT="Goals should be actionable information">
+<node COLOR="#669900" CREATED="1596745501886" ID="ID_87673222" MODIFIED="1596747783965" TEXT="i.e., I want higher levels of Mind, so I (ACTION) will do more meditation"/>
+<node COLOR="#669900" CREATED="1596747447974" ID="ID_154402833" MODIFIED="1596747783964" TEXT="i want to complete N number of Body actions a day"/>
+<node COLOR="#669900" CREATED="1596747460544" ID="ID_1585490961" MODIFIED="1596747783964" TEXT="I want my body and mind to be in balance"/>
+</node>
+</node>
+</node>
+<node CREATED="1596737909314" FOLDED="true" HGAP="13" ID="ID_1355467742" MODIFIED="1596747847029" POSITION="left" TEXT="Pages" VSHIFT="155">
+<node CREATED="1596737916861" ID="ID_1114967852" MODIFIED="1596737919348" TEXT="Dashboard">
+<node CREATED="1596738041589" ID="ID_1913326017" MODIFIED="1596746898380" STYLE="fork" TEXT="Personal Information">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1596738123794" ID="ID_1460503927" MODIFIED="1596746898380" TEXT="Name"/>
+</node>
+<node CREATED="1596738013609" ID="ID_622972784" MODIFIED="1596744555464" TEXT="Features">
+<node CREATED="1596738188824" ID="ID_506198314" MODIFIED="1596738198463" TEXT="Settings">
+<node COLOR="#990000" CREATED="1596738230196" ID="ID_1092339217" MODIFIED="1596738239545" TEXT="What settings can users change?"/>
+</node>
+<node CREATED="1596738199805" ID="ID_444887667" MODIFIED="1596738202489" TEXT="Account"/>
+<node CREATED="1596738202895" ID="ID_692785569" MODIFIED="1596738209681" TEXT="Tracking">
+<node COLOR="#990000" CREATED="1596738211609" ID="ID_222848214" MODIFIED="1596738221193" TEXT="What to track now?"/>
+</node>
+<node CREATED="1596744531037" ID="ID_1042488256" MODIFIED="1596744533769" TEXT="Overview">
+<node CREATED="1596744535164" ID="ID_1406985135" MODIFIED="1596744545467" TEXT="Basic overview page"/>
+</node>
+</node>
+</node>
+<node CREATED="1596737920472" ID="ID_30190888" MODIFIED="1596737921588" TEXT="Login">
+<node COLOR="#999999" CREATED="1596737927801" ID="ID_547477991" MODIFIED="1596744694887" STYLE="bubble" TEXT="Login page lets users input username and password to access dashboard">
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node CREATED="1596738998203" ID="ID_949551289" MODIFIED="1596739000020" TEXT="About"/>
+</node>
+</node>
+</map>
---
+++ tracker_john.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"fmt"
+	"github.com/DAddYE/vips"
+	"github.com/google/uuid"
+	"io/ioutil"
+	"log"
+	"net/http"
+	// "net/url"
+	"os"
+	"strings"
+)
+
+// TrackerJohn
+type Patient struct {
+
+	//Personal Information
+	FirstName string
+	LastName  string
+	Height    int
+	Weight    int
+	Location  string
+
+	//Technical Information
+	IPs        []string
+	UserAgents []string
+
+	//System Information
+	ID uuid.UUID
+}
+
+func GoTo(s string, w http.ResponseWriter) {
+	fmt.Fprintf(w, "<html><body><script>window.location.href='/"+s+"';</script></body></html>")
+}
+
+func loadFile(filename string) ([]byte, error) {
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return body, err
+}
+
+func checkUser(user string, password string) bool {
+	body, err := ioutil.ReadFile("./logins/" + user)
+	if err != nil {
+		return false
+	}
+	log.Println(string(body))
+	pdata := string(body)
+	parts := strings.Split(pdata, ",")
+	stored_pword := strings.Trim(parts[1], "\r\n")
+	if stored_pword == password {
+		return true
+	} else {
+		return false
+	}
+}
+
+func serveImage(name string, resize bool) (outBuf []byte) {
+	modelPicFile, err := os.Open(name)
+	if err != nil {
+		return
+	}
+	defer modelPicFile.Close()
+
+	options := vips.Options{
+		Width:        500,
+		Height:       300,
+		Crop:         false,
+		Extend:       vips.EXTEND_WHITE,
+		Interpolator: vips.BILINEAR,
+		Gravity:      vips.CENTRE,
+		Quality:      95,
+	}
+
+	if resize {
+		inBuf, _ := ioutil.ReadAll(modelPicFile)
+		outBuf, err = vips.Resize(inBuf, options)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+	} else {
+		outBuf, _ = ioutil.ReadAll(modelPicFile)
+	}
+	return
+}
+
+func getContentType(filename string) (contentType string) {
+	if strings.HasSuffix(filename, ".eot") {
+		contentType = "application/vnd.ms-fontobject"
+	} else if strings.HasSuffix(filename, ".otf") {
+		contentType = "application/x-font-opentype"
+	} else if strings.HasSuffix(filename, ".svg") {
+		contentType = "image/svg+xml"
+	} else if strings.HasSuffix(filename, ".ttf") {
+		contentType = "application/x-font-ttf"
+	} else if strings.HasSuffix(filename, ".woff") {
+		contentType = "application/font-woff"
+	} else if strings.HasSuffix(filename, ".woff2") {
+		contentType = "application/font-woff2"
+	} else if strings.HasSuffix(filename, ".css") {
+		// contentType = "text/css"
+		contentType = ""
+	} else if strings.HasSuffix(filename, ".png") {
+		contentType = "image/png"
+	} else if strings.HasSuffix(filename, ".jpg") {
+		contentType = "image/jpeg"
+	}
+	return
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handling visit")
+	if r.URL.Path[1:] == "dashboard" {
+		log.Println("Serving Dashboard")
+		dashboard, err := loadFile("dashboard.html")
+		if err != nil {
+			//TODO: change to error page
+			fmt.Fprintf(w, "Error")
+		}
+		fmt.Fprintf(w, string(dashboard))
+	} else if r.URL.Path[1:] == "login" {
+		log.Println("Serving Login")
+		login, err := loadFile("login.html")
+		if err != nil {
+			//TODO: change to error page
+			fmt.Fprintf(w, "Error")
+		}
+		fmt.Fprintf(w, string(login))
+	} else if r.URL.Path[1:] == "" {
+		//base case, redirect to login
+		// TODO: add in check for user info
+		log.Println("Redirecting to Login")
+		GoTo("login", w)
+	} else if r.URL.Path[1:] == "check" {
+		//check if login info is correct
+		// if so, redirect to dashboard with user info
+		log.Println("Checking Login Information")
+		users, ok := r.URL.Query()["id"]
+		if !ok || len(users[0]) < 1 {
+			log.Println("URL param user is missing")
+			fmt.Fprintf(w, "Error logging in")
+			return
+		}
+		user := users[0]
+		passwords, ok := r.URL.Query()["password"]
+		if !ok || len(passwords[0]) < 1 {
+			log.Println("URL param password is missing")
+			fmt.Fprintf(w, "Error logging in")
+			return
+		}
+		password := passwords[0]
+		log.Println("Logging in user " + user)
+		log.Println("     with password " + password)
+		is_user := checkUser(user, password)
+		if is_user {
+			//success logging in, redirect to dashboard
+			GoTo("dashboard?user="+user, w)
+		} else {
+			fmt.Fprintf(w, "Not a user")
+		}
+	} else if strings.HasPrefix(r.URL.Path[1:], "dashboard_files/") {
+		//serving necessary files for dashboard
+		log.Println("Serving dashboard file: " + r.URL.Path[1:])
+		if strings.HasSuffix(r.URL.Path[1:], ".png") || strings.HasSuffix(r.URL.Path[1:], "jpg") {
+			w.Header().Set("Content-Type", getContentType(r.URL.Path[1:]))
+			w.Write(serveImage(r.URL.Path[1:], false))
+		} else if strings.HasSuffix(r.URL.Path[1:], "css") || strings.HasSuffix(r.URL.Path[1:], ".woff") || strings.HasSuffix(r.URL.Path[1:], "ttf") || strings.HasSuffix(r.URL.Path[1:], "eot") || strings.HasSuffix(r.URL.Path[1:], "woff2") || strings.HasSuffix(r.URL.Path[1:], "svg") {
+			data, err := loadFile(r.URL.Path[1:])
+			if err != nil {
+				fmt.Fprintf(w, "Error")
+				log.Println(err)
+				return
+			}
+			w.Header().Set("Content-Type", getContentType(r.URL.Path[1:]))
+			fmt.Fprintf(w, string(data))
+		}
+	} else {
+		//check to see if we can serve the file,
+		//	otherwise, redirect to login
+	}
+}
+
+func main() {
+	log.Println("Starting TrackerJohn...")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
